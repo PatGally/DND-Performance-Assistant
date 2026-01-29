e59eaa75c0c03acff2cabf02d975c3af022b0f2d
PatGally <149409313+PatGally@users.noreply.github.com>
2026-01-28 20:35:30 -0600

    Refactor Druid Class
    Refactored the Druid class to pull from the monsters list.

--- CoreEngine/DNDClasses/Druid.py
+++ CoreEngine/DNDClasses/Druid.py
@@ -1,4 +1,8 @@
+import json
 from CoreEngine import Player, Monster
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MONSTER_LIST_FILE = os.path.join(BASE_DIR, "..", "data", "monster_list_NEW.json")
 
 class Druid(Player):
     def __init__(self, name, stats, saveProfs, ac, hp, class_type, level, conImmunities, damImmunes, damResists,
@@ -7,9 +11,45 @@ class Druid(Player):
                          damVulns, activeStatusEffects, activeConditions)
         self.__monster = None
 
-    def setWildShape(self, name, cr, cType, stats, hp, maxHP, ac, saveProfs, lResists, damResists, damImmunes, damVulns,conImmunes, activeConditions, activeStatusEffects, lairAction, magicResist,enemy, actions, spellInfo, legActions):
-        self.__monster = Monster(name, cr, cType, stats, hp, maxHP, ac, saveProfs, lResists, damResists, damImmunes, damVulns,conImmunes, activeConditions, activeStatusEffects, lairAction, magicResist,enemy, actions, spellInfo, legActions)
+    def setWildShape(self, name):
+        monster = self.loadMonsterStats(name)
+
+        activeConditions = ""
+        activeStatusEffects = ""
+        magicResist = False
+        enemy = False
+        actions = []
+        spellInfo = None
+
+        name = monster["name"]
+        cr = int(monster["cr"])
+        cType = monster["creatureType"]
+        stats = monster["statArray"]
+        maxHP = int(monster["hit_points"])
+        hp = maxHP
+        ac = int(monster["AC"])
+        saveProfs = monster["saveProfs"]
+        lResists = monster["lResists"]
+        damResists = monster["damResists"]
+        damImmunes = monster["damImmunes"]
+        damVulns = monster["damVulns"]
+        conImmunes = monster["conImmunes"]
+        lairAction = monster["lairAction"]
+        legActions = monster["legAction"]
+
+        self.__monster = Monster(name, cr, cType, stats,hp, maxHP, ac, saveProfs,lResists, damResists,
+                                damImmunes, damVulns, conImmunes,activeConditions, activeStatusEffects,
+                                 lairAction, magicResist, enemy,actions, spellInfo, legActions)
+
+    def loadMonsterStats(self, wildShapeName):
+        with open(MONSTER_LIST_FILE, "r") as f:
+            monsters = json.load(f)
+
+        for monster in monsters:
+            if monster["name"] == wildShapeName:
+                return monster
+
+        raise ValueError(f"Monster '{wildShapeName}' not found")
 
     def getWildShape(self):
         return self.__monster
-
