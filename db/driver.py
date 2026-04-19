2fc9aeb7f0f6b59f4b8f7c3164196dc1b1a3e015
PatGally <149409313+PatGally@users.noreply.github.com>
2026-04-19 13:37:43 -0500

    DB Bug Issue fixed

--- db/driver.py
+++ db/driver.py
@@ -1,32 +1,27 @@
 import os
 import urllib.parse
-from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.server_api import ServerApi
 
 
 class Driver:
-    _client = None
-    _db = None
-
     def __init__(self):
-        load_dotenv(".env")
+        self._client = None
+        self._db = None
 
         env = os.getenv("ENV", "development").lower()
 
         if env == "production":
-            load_dotenv(".env.production", override=True)
             username = os.getenv("MONGO_PROD_USER")
             raw_password = os.getenv("MONGO_PROD_PASSWORD")
         else:
-            load_dotenv(".env.development", override=True)
             username = os.getenv("MONGO_USER")
             raw_password = os.getenv("MONGO_PASS")
 
         db_name = os.getenv("MONGO_DB_NAME", "dndpa")
 
         if not username or not raw_password:
-            raise RuntimeError("MongoDB credentials not found")
+            raise RuntimeError(f"MongoDB credentials not found for ENV={env}")
 
         password = urllib.parse.quote_plus(raw_password)
 
@@ -35,19 +30,21 @@ class Driver:
             f"@cluster0.dcn6d.mongodb.net/?appName=Cluster0"
         )
         self._db_name = db_name
+        self._env = env
 
     def connect(self):
-        if Driver._client is None:
-            Driver._client = AsyncIOMotorClient(
+        if self._client is None:
+            print(f"[mongo] ENV={self._env}, DB={self._db_name}")
+            self._client = AsyncIOMotorClient(
                 self._uri,
                 server_api=ServerApi("1"),
             )
-        return Driver._client
+        return self._client
 
     def get_db(self):
-        if Driver._db is None:
-            Driver._db = self.connect()[self._db_name]
-        return Driver._db
+        if self._db is None:
+            self._db = self.connect()[self._db_name]
+        return self._db
 
     def get_collection(self, name: str):
         return self.get_db()[name]
